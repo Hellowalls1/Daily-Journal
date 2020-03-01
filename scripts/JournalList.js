fe563a3f6d3ee9b100fdde6bde9d1a3fce8a8f09
Hellowalls1 <thesingingbutcher@gmail.com>
2020-03-01 10:02:33 -0600

    updated journal entry page

---
+++ scripts/JournalList.js
@@ -0,0 +1,26 @@
+/*
+ *  Purpose:
+ *    To render as many journal entry components as
+ *    there are items in the collection exposed by the
+ *    data provider component
+ */
+import { useJournalEntries } from "./JournalDataProvider.js" //function used to sort array of objects by date
+import JournalEntryComponent from "./JournalEntry.js" //function used to express HTML 
+
+// DOM reference to where all entries will be rendered
+const entryLog = document.querySelector("#entryLog") //used to send HTML to location on entrylog with "id=entryLog" now defined by entryLog
+
+const EntryListComponent = () => {
+    // Use the journal entry data from the data provider component
+    const entries = useJournalEntries()
+
+    for (const entry of entries) { //for every individual object in the entries(useJournalEntries) function array
+        /*
+            Invoke the component that returns an
+            HTML representation of a single entry
+        */
+        entryLog.innerHTML += JournalEntryComponent(entry); //log each entry (individual object of entries array) into the HTML @ #entryLog
+}
+}
+
+export default EntryListComponent
