0b98bc4878c7f76a304638f4cc8cdcdb3788c69f
Hellowalls1 <thesingingbutcher@gmail.com>
2020-03-18 14:50:05 -0500

    form on screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import EntryListComponent  from "./JournalList.js";
 import { getEntries, useJournalEntries } from "./JournalDataProvider.js";
-
+import JournalForm from "./JournalForm.js";
+JournalForm()
 getEntries().then(
     () => {
         EntryListComponent()
